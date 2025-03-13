7bb132bd6390cce645b7943f6fcfe6a6b984aab9
D <pogrebnjak_dima@mail.ru>
2025-03-13 17:50:32 +0600

    2

---
+++ euroblock/Views/Home/index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace euroblock.Views.Home
+{
+    public class IndexModel : PageModel
+    {
+        public List<string> AdImages { get; set; }
+
+        public void OnGet()
+        {
+            var imagesPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images");
+            AdImages = Directory.GetFiles(imagesPath, "ad*.jpg")
+                                .Select(Path.GetFileName)
+                                .ToList();
+        }
+    }
+}
